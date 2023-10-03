bcddefa302d34a51913cacf9d9f950b453262d84
Siddharth-Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2023-10-02 21:47:00 -0400

    added ping controller

---
+++ src/main/java/com/unispace/lms/controller/PingController.java
@@ -0,0 +1,13 @@
+package com.unispace.lms.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PingController {
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
